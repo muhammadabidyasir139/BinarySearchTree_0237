2362cd8c092ca693a8ce7e3929e4195245745cde
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-06-13 15:55:03 +0700

    membuat logic preorder

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -83,4 +83,19 @@ class BinaryTree
                 inorder(ptr->rightchild);
             }
         }
-}
+        void preorder(Node* ptr)
+        {
+            if (ROOT = NULL)
+            {
+                cout << "Tree is empty" << endl;
+                return;
+            }
+            if (ptr != NULL)
+            {
+                cout << ptr->info << "";
+                preorder(ptr->leftchild);
+                preorder(ptr->rightchild);
+            }
+        }
+        
+};
