d6c2585dc63f1e0cc309cca72290d88202677b25
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-06-13 15:48:59 +0700

    membuat flow inorder lengkap

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -72,5 +72,15 @@ class BinaryTree
         void inorder(Node* ptr)
         {
             if (ROOT = NULL)
+            {
+                cout << "Tree is empty" << endl;
+                return;
+            }
+            if (ptr != NULL)
+            {
+                inorder(ptr->leftchild);
+                cout << ptr->info << "";
+                inorder(ptr->rightchild);
+            }
         }
 }
