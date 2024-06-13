b2e34b3c85994cfc04a4bcdebaf9937c50823c74
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-06-13 16:07:08 +0700

    membuat tampilan untuk memilih

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -134,7 +134,30 @@ int main() {
             case '1':
             {
                 cout << "Enter a word: ";
+                string word;
+                cin >> word;
+                x.insert(word);
+                break;
             }
+            case '2':
+            {
+                x.inorder(x.ROOT);
+                break;
+            }
+            case '3':
+            {
+                x.preorder(x.ROOT);
+                break;
+            }
+            case '4':
+            {
+                x.postorder(x.ROOT);
+                break;
+            }
+            case '5':
+            {
+                return;
+            } 
         }
     }
 }
